322c630b8f79357d8d7be2c7340ddfab5c55cc28
mfeemster <matt.feemster@gmail.com>
2016-04-21 18:43:52 -0700

    --Code changes  -Pass XML_PARSE_HUGE to xmlReadMemory() to allow for reading large Xml files.

--- Source/Ember/XmlToEmber.h
+++ Source/Ember/XmlToEmber.h
@@ -320,7 +320,7 @@ public:
 		xmlPtr = CX(&buf[0]);
 		bufSize = strlen(xmlPtr);
 		//embers.reserve(bufSize / 2500);//The Xml text for an ember is around 2500 bytes, but can be much more. Pre-allocate to aovid unnecessary resizing.
-		doc = xmlReadMemory(xmlPtr, int(bufSize), filename, "ISO-8859-1", XML_PARSE_NONET);//Forbid network access during read.
+		doc = xmlReadMemory(xmlPtr, int(bufSize), filename, "ISO-8859-1", XML_PARSE_HUGE);
 		//t.Toc("xmlReadMemory");
 
 		if (doc == nullptr)
